7b1ef369d5d75fe2d668ea54cc5849d3ce4af04b
JayLee07 <wpgur0107@gmail.com>
2018-09-05 18:42:46 +0900

    add initialization method

--- model_utils.py
+++ model_utils.py
@@ -15,6 +15,27 @@ def initialize_weights(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
+def resnet_initialize_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+    elif classname.find('BatchNorm2d') != -1:
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+
+def vgg_initialize_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif classname.find('BatchNorm2d') != -1:
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+    # elif isinstance(m, nn.Linear):
+    #     nn.init.normal_(m.weight, 0, 0.01)
+    #     nn.init.constant_(m.bias, 0)
+    
 def get_norm_layer(norm_type='batch'):
     if norm_type == 'batch':
         norm_layer = nn.BatchNorm2d
