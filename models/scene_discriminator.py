f43ee218df8a1b622d2451608a653369dc2d5bc4
JayLee07 <wpgur0107@gmail.com>
2018-09-05 18:50:21 +0900

    modify scene_discriminator.py

--- models/scene_discriminator.py
+++ models/scene_discriminator.py
@@ -7,11 +7,11 @@ class Scene_discriminator(nn.Module):
         super(Scene_discriminator, self).__init__()
         self.args = args
         self.layers = nn.Sequential(nn.Linear(self.args.pose_dim * 2, self.args.sceneD_hidden_layers),
-                                                nn.ReLU(inplace=True),
-                                                nn.Linear(args.sceneD_hidden_layers, args.sceneD_hidden_layers),
-                                                nn.ReLU(inplace=True),
-                                                nn.Linear(args.sceneD_hidden_layers, 1),
-                                                nn.Sigmoid())
+                                    nn.ReLU(inplace=True),
+                                    nn.Linear(args.sceneD_hidden_layers, args.sceneD_hidden_layers),
+                                    nn.ReLU(inplace=True),
+                                    nn.Linear(args.sceneD_hidden_layers, 1),
+                                    nn.Sigmoid())
     
     def forward(self, x):
         output = self.layers(torch.cat(x, 1).view(-1, self.args.pose_dim * 2))
